96e32344ebe872cad9c6de93795167434b33a37d
Enrique Acon Soto <enrique.acon@ecix.tech>
2025-12-18 18:02:59 +0100

    pruebafinal

--- wsgi.py
+++ wsgi.py
@@ -6,8 +6,11 @@ APP_DIR = "/var/www/alumnos/eacon/flask_app"
 if APP_DIR not in sys.path:
     sys.path.insert(0, APP_DIR)
 
-os.environ.setdefault("MONGODB_URI", "mongodb://localhost:27017/")
+os.environ.setdefault("MONGODB_URI", "mongodb+srv://kikerivas96_db_user:emmkyTLUNmyA1xw2@cluster0.1umwjrf.mongodb.net/?appName=Cluster0")
 os.environ.setdefault("MONGODB_DB", "Clinica")
 
 # Importamos la app de Flask desde application.py
 from application import app as application
+
+
+ 
